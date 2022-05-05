12aa62cb68d2881decad92ab2175cd6f0382df95
motlaghreyhan <rmotlagh@Reyhans-MacBook-Pro.local>
2022-05-04 21:50:07 -0400

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp -rf bashrc ~/.bashrc
+cp -rf tmux ~/.tmux
+mv ~/.tmux.conf ~/.tmux.conf.bak
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+cp -rf vim ~/.vim
+cp -rf vim_runtime ~/.vim_runtime
+cp -rf viminfo ~/.viminfo
+cp -rf vimrc ~/.vimrc
+
+source ~/.bashrc
