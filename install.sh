4d4bb5871a314b319c5129239058ae906f1f602d
motlaghreyhan <88353058+motlaghreyhan@users.noreply.github.com>
2022-06-03 20:15:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp -rf tmux/* ~/.tmux
 unlink ~/.tmux.conf
 ln -s ~/.tmux/tmux.conf ~/.tmux.conf
 cp -rf vim/* ~/.vim
-cp -rf vim_runtime/* ~/.vim_runtime
+cp -rf vim_runtime ~/.vim_runtime
 cp -f viminfo ~/.viminfo
 cp -f vimrc ~/.vimrc
 
