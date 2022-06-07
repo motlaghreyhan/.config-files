d7ee28c2e7a985aa88c3d331c1454e09eee00af0
motlagh.2 <motlagh.2@mri.cluster>
2022-06-07 13:20:07 -0500

    editing main to fix issues with non macbook devices

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+rm -rf ~/.bashrc
 cp -f bashrc ~/.bashrc
-cp -rf tmux/* ~/.tmux
+rm -rf ~/.tmux
+cp -rf tmux ~/.tmux
 unlink ~/.tmux.conf
 ln -s ~/.tmux/tmux.conf ~/.tmux.conf
-cp -rf vim/* ~/.vim
+rm -rf ~/.vim
+cp -rf vim ~/.vim
+rm -rf ~/.vim_runtime
 cp -rf vim_runtime ~/.vim_runtime
+rm -rf ~/.viminfo
 cp -f viminfo ~/.viminfo
+rm -rf ~/.vimrc
 cp -f vimrc ~/.vimrc
 
 source ~/.bashrc
